33173811a4c1b53b4a013b0f9bbeba3f03a458df
Soraaa <dpanganiban@gbox.ncf.edu.ph>
2026-05-15 21:31:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showMessage() {
+    alert(`Happy Birthday, Mahal! 💖
+
+Tomorrow is your day, and I want you to know how much you mean to me. Imagine, it’s only been 4 months since we started talking, but look at us now we both happy that we met each other. I fell in love with you so fast, and I don't regret a single second of it.
+
+I know life can be unfair sometimes, but please never give up. I hope this year treats you with the kindness you deserve. Mag-iingat kayo ni Mama palagi, lalo na sa mga pagbabyahe niyo ngayong taon.
+
+Good luck sa acads mo ngayong pasukan! Galingan mo ha? And please, wag kang mahihiyang lumapit sa akin kung may kailangan ka. If it’s something I can provide, I’ll be there for you.
+
+Kahit na madalas tayong magaway at magbati, sana walang bumitaw sating dalawa Huhu. I hope this relationship never ends. My dream is for us to stay together until we’re both old and our hair is white! HAHAHHA.
+
+Sana wag na masyadong pasaway ha? My wish for you is to have a good life and a beautiful year ahead. Good luck to your new journey this year! Kahit makulit ka, mahal na mahal kita. Happy Birthday, Mahal!`);
+
+ }
